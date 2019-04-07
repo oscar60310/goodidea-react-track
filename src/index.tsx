f79d4fc21b11ede4fcb05f06fd80c7a36b019078
Ivan Tsai <oscar60310@gmail.com>
2019-04-07 21:15:32 +0800

    add Q4 site

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-/*global document */
-import * as React from 'react';
-import { render } from 'react-dom';
-// import styles
-import './styles/index.scss'
-import AppComponent from './components/app';
-
-const app = document.getElementById('app');
-render(<AppComponent />, app);
